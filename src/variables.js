5642100392792e5c09b966a5e60b5b1c2620c099
Khairul Abdi Dongoran <38163228+khairul-abdi@users.noreply.github.com>
2019-05-24 22:38:33 +0700

    2.2.data-types
    Mengenal Data Types

--- src/variables.js
+++ src/variables.js
@@ -31,3 +31,48 @@ Hindari menggunakan var
 sebaiknya menggunakan let
 dan gunakan conts yang untuk nilai tidak akan berubah
 */
+
+// ===================================
+//-----------Data Types------------
+/*
+1. Primitives :
+    String, Number, Boolean, Null, Undefined, Symbol
+2. Object
+*/
+
+const socialMedia1 = {
+  instagram: "@budi_ig1",
+  facebook: "@budi_fb1"
+};
+
+console.log(socialMedia1.instagram, typeof socialMedia1);
+console.log(socialMedia1.facebook, typeof socialMedia1);
+
+const hobbies1 = ["music", "dancing", "sleeping"];
+console.log(hobbies1[0], typeof hobbies1);
+console.log(hobbies1[1], typeof hobbies1);
+console.log(hobbies1[2], typeof hobbies1);
+
+//Tambahan (Memperdalam API JSON) seperti dibawah
+
+let socialMedia = {
+  instagram: "@budi_ig",
+  facebook: "@budi_fb",
+  hobbies: [
+    {
+      music: "",
+      dancing: "",
+      sleeping: "",
+      gamer: {
+        mobile: "AOV",
+        PC: "Call Of Duty"
+      }
+    }
+  ]
+};
+
+console.log(socialMedia.instagram, typeof socialMedia);
+console.log(socialMedia.facebook, typeof socialMedia);
+console.log(socialMedia.hobbies[0].gamer.mobile);
+// console.log(socialMedia.hobbies[3].PC);
+console.log(socialMedia.hobbies[0].gamer.PC);
